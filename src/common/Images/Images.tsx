925a7dc7af2a4ce94d13d150f96dfef3a5d9faaf
uday <udaysinghp95@gmail.com>
2022-07-28 19:38:42 +0530

    changes to folder

--- src/common/Images/Images.tsx
+++ src/common/Images/Images.tsx
@@ -6,6 +6,7 @@ import { setImageVisile } from "../../store/action";
 import Photo from "../../types/Photos";
 import State from "../../store/types/State";
 import getName from "../../utils/getName";
+import ContentLoader from "react-content-loader";
 
 type PropsType = {
 	children?: React.ReactNode;
@@ -18,31 +19,30 @@ function Images({ children }: PropsType) {
 	const observer = useRef<IntersectionObserver>();
 	const curr = useSelector(({ path }: State) => path[path.length - 1]);
 
-	useEffect(() => {
-		console.log("changes images", curr);
-
-		setList([]);
-	}, [curr]);
-
-	console.log(curr);
+	useEffect(() => setList([]), [curr]);
 
 	const fetchImages = async () => {
-		console.log("fetching " + curr);
-
 		try {
 			setLoading(1);
+
+			console.log("fetching", curr);
 
 			const {
 				data: { results },
 			} = await axios.get(
 				`https://api.unsplash.com/search/photos/?query=${getName(
 					curr
-				)}&page=${page}&per_page=21&orientation=landscape&client_id=H8VJv2yEWzXwlRYpjUT_dx88geTwpCYnyRUcUZvQgM4`
+				)}&page=${page}&per_page=20&orientation=landscape&client_id=H8VJv2yEWzXwlRYpjUT_dx88geTwpCYnyRUcUZvQgM4`
 			);
 
-			const newList = [...list];
+			let newList: string[] = [];
 
-			results.map((img: Photo) => newList.push(img.urls.thumb as string));
+			console.log(curr, list[0]?.includes(getName(curr)));
+
+			if (list.length != 0 && list[0].includes(getName(curr)))
+				newList = [...list];
+
+			results.map((img: Photo) => newList.push(img.urls.small as string));
 
 			setPage(page + 1);
 
@@ -69,18 +69,19 @@ function Images({ children }: PropsType) {
 
 			if (node && loading === 0) observer.current.observe(node);
 		},
-		[loading]
+		[loading, curr]
 	);
 
 	const dispatch = useDispatch();
 
 	const handleClick = (img: string) =>
-		dispatch(setImageVisile(img.replace("w=200", "w=1080")));
+		dispatch(setImageVisile(img.replace("w=400", "w=1080")));
 
 	return (
 		<div className="im26ctn">
 			<div className="im26sc">
 				{children}
+
 				{list.map((url, index) => (
 					<img
 						src={url}
@@ -91,17 +92,32 @@ function Images({ children }: PropsType) {
 					/>
 				))}
 
-				{loading === 0 && <div ref={lastElementRef} />}
+				{loading !== 2 && (
+					<>
+						<div ref={lastElementRef} />
+						{[...Array(20)].map((d, index) => (
+							<ContentLoader
+								key={index}
+								width="18rem"
+								height="13rem"
+								className="im26ld"
+							>
+								<rect width="100%" height="100%" />
+							</ContentLoader>
+						))}
+					</>
+				)}
 			</div>
 
-			{loading !== 0 && (
-				<div className="im26st">
-					{loading === 1 && <img src="/loading.svg" alt="loading state" />}
-					{loading === 2 && (
-						<img src="/error.gif" alt="error state" className="im26er" />
-					)}
-				</div>
-			)}
+			<div className="im26st">
+				{loading === 2 && (
+					<img
+						src={process.env.PUBLIC_URL + "/error.gif"}
+						alt="error state"
+						className="im26er"
+					/>
+				)}
+			</div>
 		</div>
 	);
 }
